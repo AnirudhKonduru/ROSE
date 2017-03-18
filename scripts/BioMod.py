90356e2d96b1e1d7bfad742c86a8dfa2df20835a
Anirudh K M <anirudhmkonduru@gmail.com>
2017-03-19 03:01:50 +0530

    added read_fasta_file(), re-added seq2regex(), major bug fixes, minor refractoring

--- scripts/BioMod.py
+++ scripts/BioMod.py
@@ -1,13 +1,27 @@
 import regex as re
 
 class Seq(object):
-    def __init__(self,id,seq, chr=-1, pos=-1, pos_end=-1):
+    def __init__(self,id,seq, chr=-1, pos=-1, pos_end=-1,header_string=""):
         self.seq = seq
         self.id = id
         self.chr = chr
         self.pos = pos
-        self.pos_end = -1;
+        self.pos_end = -1
+        self.header_string = header_string
+        if header_string=="":
+            self.header_string=str(self.id)+" "+str(self.chr)+" "+str(self.pos)+'\n'
 
+    def __str__(self):
+        res = self.header_string+'\n'
+        if self.len()<20:
+            res+=self.seq
+        else:
+            res +=self.seq[:10]+"......"+self.seq[-10:]
+        return res
+
+    def __repr(self):
+        return self.__str__()
+#converts singe fasta string to Seq object
     @classmethod
     def from_fasta(cls, fasta):
         lines = fasta.split("\n")
@@ -18,10 +32,10 @@ class Seq(object):
         #remove > at the beginning
         header_items[0] = header_items[0][1:]
         id = header_items[0]
-        if len(header_items)==3:
+        if len(header_items)>=3:
             chr = int(header_items[1])
             pos = int(header_items[2])
-            return Seq(id,seq,chr,pos)
+            return Seq(id,seq,chr,pos, header_string=lines[0])
         else:
             return Seq(id,seq)
 
@@ -34,18 +48,24 @@ class Seq(object):
     def len(self):
         return len(self.seq)
 
-def read_fasta_file(file_name):
-    with open(file_name, "r") as f:
-        fastas = []
-        new = "default"
-        for line in f:
-            if line[0]=='>':
-                fastas.append(new)
-                new=line
-            else:
-                new = new+line
-        fastas.remove("default")
-    return fastas
+#converts string with iupac nucleotide codes to regex strings
+    def regex(self):
+        return seq2regex(self.seq)
+
+def seq2regex(seq):
+    regex_seq = regex_seq
+    regex_seq = regex_seq.replace('R','[AG]')
+    regex_seq = regex_seq.replace('Y','[CT]')
+    regex_seq = regex_seq.replace('S','[GC]')
+    regex_seq = regex_seq.replace('W','[AT]')
+    regex_seq = regex_seq.replace('K','[GT]')
+    regex_seq = regex_seq.replace('M','[AC]')
+    regex_seq = regex_seq.replace('B','[CGT]')
+    regex_seq = regex_seq.replace('D','[AGT]')
+    regex_seq = regex_seq.replace('H','[ACT]')
+    regex_seq = regex_seq.replace('V','[ACG]')
+    regex_seq = regex_seq.replace('N', '[ATGC]')
+    return regex_seq
 
 def get_hamming(str1, str2, max_hamming=-1):
     leng = min(len(str1),len(str2))
@@ -59,25 +79,35 @@ def get_hamming(str1, str2, max_hamming=-1):
                 return -1
     return err
 
-#converts string with iupac nucleotide codes to regex strings
-def seq2regex(seq):
-    seq = seq.replace('R','[AG]')
-    seq = seq.replace('Y','[CT]')
-    seq = seq.replace('S','[GC]')
-    seq = seq.replace('W','[AT]')
-    seq = seq.replace('K','[GT]')
-    seq = seq.replace('M','[AC]')
-    seq = seq.replace('B','[CGT]')
-    seq = seq.replace('D','[AGT]')
-    seq = seq.replace('H','[ACT]')
-    seq = seq.replace('V','[ACG]')
-    seq = seq.replace('N', '[ATGC]')
-    return seq
 
 #finds all occurences of needle in haystack
 #returns list of start positions in haystack string
 def findSeq(needle, haystack, regex=True):
     if regex==True:
-        needle_r = re.compile(seq2regex(needle))
-        matches = needle_r.finditer(haystack, overlapped=True)
+        needle_r = re.compile(needle.regex())
+        matches = needle_r.finditer(haystack.seq, overlapped=True)
         return [x.start() for x in matches]
+
+def read_fasta(fasta_str=""):
+    #reads and returns a list of strings,
+    #each string is one fasta Entry
+    fastas = []
+    new = "default"
+    lines = [x+'\n' for x in fasta_str.split('\n')]
+    for line in lines:
+        if line=="\n" or line=="":
+            continue
+        if line[0]=='>':
+            fastas.append(new)
+            new=line
+        else:
+            new = new+line
+    fastas.append(new)
+    fastas.remove("default")
+    #converts each fasta string to Seq object, returhs list
+    return [Seq.from_fasta(x) for x in fastas]
+
+def read_fasta_file(fasta_file):
+        f = open(fasta_file, 'r')
+        file_str = f.read()
+        return read_fasta(file_str)
